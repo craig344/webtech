000118411e38874add101bdfb6eac9c91c229eab
Craig Da Silva <craig_344@hotmail.com>
2019-02-07 11:51:41 +0530

    V3 init

---
+++ EX Submit Table V3/main.js
@@ -0,0 +1,39 @@
+function showInput(event) {
+    event.preventDefault();
+    let elements = document.getElementById("form").elements;
+    var table = document.getElementById("myTable");
+    var row = table.insertRow();
+    var cell;
+    var i,j,k;
+
+
+    for(i=0; i<elements.length; i++){
+        if(elements[i].type == "text"){
+            //console.log(elements[i].value);
+            cell = row.insertCell(i);
+            cell.innerHTML = elements[i].value;
+        }
+        else if(elements[i].type=="select-multiple"){
+            cell = row.insertCell(i);
+            let sel = document.getElementById("sub");
+            for(j=0; j<sel.options.length; j++){
+                if(sel.options[j].selected){
+                    //console.log(sel.options[j].value);
+                    cell.innerHTML += sel.options[j].value;
+                    for(k=j+1; k<sel.options.length; k++){
+                        if(sel.options[k].selected){
+                            cell.innerHTML += ", "
+                        }
+                    }
+                }
+            }
+        }
+        else if(elements[i].checked){
+            //console.log(elements[i].value);
+            cell = row.insertCell(k);
+            cell.innerHTML = elements[i].value;
+        }
+
+        
+    }
+}
