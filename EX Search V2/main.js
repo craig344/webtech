d170585ba529a0f543f5511f9df24a6f96101a13
Craig Da Silva <craig_344@hotmail.com>
2019-02-18 11:25:20 +0530

    init Search v2

---
+++ EX Search V2/main.js
@@ -0,0 +1,12 @@
+function searchWords(event) {
+    event.preventDefault();
+    var para =  document.getElementById("para")
+    var inputText = para.innerHTML;
+    var sValue = document.getElementById("searchw").value;
+    var length = sValue.length;
+    var pos = inputText.search(sValue);
+    var found = inputText.slice(pos,pos+length);
+    if(found.length > 0){
+        para.innerHTML = inputText.substr(0,pos) + "<span class='highlight'>" + found + "</span>" + inputText.substr(pos+length,para.length);
+    }
+}
